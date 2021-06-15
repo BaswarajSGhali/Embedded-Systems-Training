acd1768a48d36f04b208e281bc3ce6c6a4aaa7f3
BaswarajSGhali <46929810+BaswarajSGhali@users.noreply.github.com>
2021-06-15 14:25:45 +0530

    Update Program3.c

--- Assignment_2/Program3.c
+++ Assignment_2/Program3.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+	int x = 10, y = 3;
+
+	{
+		int x = 100, z = 10;
+		printf("%d %d\n", x, z);
+	}
+
+	printf("%d %d\n", x, z);    // z does'nt exist
+	return 0;
+}
+
+// the output of the above code is error, because after the bracket z does'nt exist
