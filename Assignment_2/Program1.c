b54d5201f0f263c7933c0fb2bae61386483fb176
BaswarajSGhali <46929810+BaswarajSGhali@users.noreply.github.com>
2021-06-15 14:25:01 +0530

    Update Program1.c

--- Assignment_2/Program1.c
+++ Assignment_2/Program1.c
@@ -1,1 +1,15 @@
+// This program takes input from the use (i.e.) password
 
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 2, b = 1, x;
+
+	x = (a++ && (!b) && b || ++a);
+
+	printf("%d", x);
+
+	return 0;
+}
