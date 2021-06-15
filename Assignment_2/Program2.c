24fe78a08da0a2aaa3de66c0b5896371648cb4b4
BaswarajSGhali <46929810+BaswarajSGhali@users.noreply.github.com>
2021-06-15 14:25:24 +0530

    Update Program2.c

--- Assignment_2/Program2.c
+++ Assignment_2/Program2.c
@@ -1,1 +1,49 @@
+#include<stdio.h>
+
+int sum_of_digits(int n);
+
+int main()
+{
+
+	int n, result;
+	printf("Enter the number n\n");
+	scanf("%d", &n);
+	result = n;
+
+	if(n < 10)
+	{
+		printf("sum is %d", n);
+	}
+
+	else
+	{
+		while(result >= 10)
+		{
+			n = result;
+			result = sum_of_digits(n);
+		}
+
+		printf("sum is %d", result);
+	}
+
+	return 0;
+
+}
+
+
+int sum_of_digits(int n)
+{
+	int sum = 0, temp;
+
+	while(n > 0)
+	{
+		temp = n;
+		n = n % 10;
+		sum = sum + n;
+		temp = temp / 10;
+		n = temp;
+	}
+
+	return sum;
+}
 
