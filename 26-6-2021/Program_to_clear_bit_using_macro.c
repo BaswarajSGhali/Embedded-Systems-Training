caf041836ae8d5ea7d6de5aa11f005c7932e79aa
BaswarajSGhali <46929810+BaswarajSGhali@users.noreply.github.com>
2021-06-29 14:58:59 +0530

    Add files via upload

---
+++ 26-6-2021/Program_to_clear_bit_using_macro.c
@@ -0,0 +1,23 @@
+/* The \ at the end of each line is to signal line continuation
+   (i.e. to tell the compiler "this macro continues on the next line")
+*/
+
+#include<stdio.h>
+
+#define check(x, y)                                     \
+({                                                      \
+      num = ((num) & ~(1 << pos));                  \
+      printf("\n0x%x\n", num);                              \
+})
+
+int main()
+{
+  int num = 0x12345678;
+  printf("0x%x\n", num);
+  int pos = 3;
+  int result;
+
+  check(num, pos);
+}
+
+
